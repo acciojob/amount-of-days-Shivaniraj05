80a50655385a7313669f55f168ec87274239c34a
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-04-30 21:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function daysOfAYear(int year){
+
+	if(year % 4 === 0){
+		if(year % 100 === 0){
+			return year%400 === 0 ? 366:365;
+		}
+
+		else return 366;
+	}
+
+	return 365;
+	
+}
